2efb720b249da768a508c4bc2bd68856ca981891
alexgomezalanis <alexgomezalanis@gmail.com>
2020-05-15 20:26:38 +0200

    Run features

--- run_features.sh
+++ run_features.sh
@@ -5,7 +5,7 @@ echo 'Current Dir: '${RootDir}
 extractLPS=1
 if [ $extractLPS -eq 1 ]; then
     echo 'Extracting Log Power Spectrograms ...'
-    python ./preprocess/compute_LPS.py --in_dir /home/sxliu/projects/asvspoof/asvspoof2019_data/ \
+    python ./preprocess/compute_LPS.py --in_dir /home2/alexgomezalanis/asvspoof_2019_database/ \
                                        --out_dir ./data_LA/LPS_863 \
                                        --access_type LA \
                                        --param_json_path ./preprocess/conf/stft_T45.json
@@ -15,9 +15,9 @@ fi
 extractLMS=0
 if [ $extractLMS -eq 1 ]; then
     echo 'Extracting Log Magnitude Spectrograms ...'
-    python ./preprocess/compute_LMS.py --in_dir /scratch/sxliu/projects/asvspoof/ASVspoof2019-data/ \
-                                       --out_dir ./data/LMS \
-                                       --access_type PA \
-                                       --param_json_path ./preprocess/conf/stft.json
+    python ./preprocess/compute_LMS.py --in_dir /home2/alexgomezalanis/asvspoof_2019_database/ \
+                                       --out_dir ./data_LA/LMS_863 \
+                                       --access_type LA \
+                                       --param_json_path ./preprocess/conf/stft_T45.json
 fi
 
